e6ce534e4abf968d737f6ca2cc115dc88772a64d
Victor Manuel Acuña <victor.acuna@servexternos.isban.cl>
2017-10-17 12:25:26 -0300

    Add shopping list edit section

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,5 +1,10 @@
 export class Ingredient {
+  public name: string;
+  public amount: number;
 
-  constructor(public name: string, piblic amount:number) {
+  constructor(name: string,
+              amount: number) {
+    this.name = name;
+    this.amount = amount;
   }
 }
